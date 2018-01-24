75729f0a669bd2ca180bb5f26bda64bd58702a59
Becks <becks.simpson@outlook.com>
2018-01-24 15:39:53 +0000

    Fixed threading, multiple people

--- videoGUI.py
+++ videoGUI.py
@@ -5,61 +5,78 @@ from PIL import Image, ImageTk
 import httplib, urllib, base64
 import json
 import ast
+from threading import Thread
+from Queue import Queue
 #PIL is the Python Imaging Library
 #The Image module provides a class with the same name which is used to represent a PIL image. 
 #The ImageTk module contains support to create and modify Tkinter BitmapImage and PhotoImage objects from PIL images.
 # The module also provides a number of factory functions, including functions to load images from files, and to create new images.
 
-def sendFace(imgFace):
-    
-    #img.tobytes()
-    #img=np.unpackbits(img)
-    #print(img)
-    imgFace=cv2.imencode('.png', imgFace)[1].tostring()
-    #print(imgFace)
-    #imgFace= base64.b64encode(imgFace)
-    #print(imgFace)
-    headers = {
-        # Request headers
-        'Content-Type': 'application/octet-stream',
-        'Ocp-Apim-Subscription-Key': '967fba0ccc95407190ab8a7522a164ab',
-    }
+        
+        
+def sendFace():
+    global currentRect, currentEmotion, emotionPics
+    while True:   
+        #img.tobytes()
+        #img=np.unpackbits(img)
+        #print(img)
+        print("Threaded sending face")
+        imgFace = q.get()
+        q.task_done()
+        print("Got queue item")
+        imgFace=cv2.imencode('.png', imgFace)[1].tostring()
+        #print(imgFace)
+        #imgFace= base64.b64encode(imgFace)
+        #print(imgFace)
+        headers = {
+            # Request headers
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': '967fba0ccc95407190ab8a7522a164ab',
+        }
 
-    params = urllib.urlencode({
-        # Request parameters
-        'returnFaceId': 'true',
-        #'returnFaceLandmarks': 'false',
-        'returnFaceAttributes': 'emotion',
-    })
+        params = urllib.urlencode({
+            # Request parameters
+            'returnFaceId': 'true',
+            #'returnFaceLandmarks': 'false',
+            'returnFaceAttributes': 'emotion',
+        })
 
-    try:
-        conn = httplib.HTTPSConnection('northeurope.api.cognitive.microsoft.com')
-        conn.request("POST", "/face/v1.0/detect?%s" % params, imgFace , headers)
-        response = conn.getresponse()
-        data = response.read()
-        #print(data)
-        data=ast.literal_eval(data)
-        #data=data.encode("ascii","replace")
-        #data=json.loads(data)
-        #data=data.encode("ascii","replace")
-        print("Received response", data)
-        rect=0
-        emotion=""
-        if(len(data)>0):
-            #print(data[0])
-            rect=data[0]['faceRectangle']
-            #imageFrame.delete("all")
+        try:
+            conn = httplib.HTTPSConnection('northeurope.api.cognitive.microsoft.com')
+            conn.request("POST", "/face/v1.0/detect?%s" % params, imgFace , headers)
+            response = conn.getresponse()
+            data = response.read()
+            #print(data)
+            data=ast.literal_eval(data)
+            #data=data.encode("ascii","replace")
+            #data=json.loads(data)
+            #data=data.encode("ascii","replace")
+            print("Received response", data)
+            rect=0
+            emotion=""
+            if(len(data)>0):
+                currentRect=[]
+                currentEmotion=[]
+                for res in data:
+                    #print(data[0])
+                    rect=res['faceRectangle']
+                    #imageFrame.delete("all")
+                    
+                    emotions= res['faceAttributes']['emotion'] 
+
+                    currentEmotion.append(max(emotions.keys(), key=(lambda k: emotions[k])))
+                    print("Strongest emotion is:", currentEmotion)
+                    #imageFrame.create_rectangle(rect['left'], rect['top'], rect['left']+rect['width'], rect['top']+rect['height'],outline="blue")
+                    #imageFrame.pack()
+
+                    currentRect.append(rect)
+                    
+                    print("Saying task complete")
             
-            emotions= data[0]['faceAttributes']['emotion'] 
-            emotion=max(emotions.keys(), key=(lambda k: emotions[k]))
-            print("Strongest emotion is:", emotion)
-            #imageFrame.create_rectangle(rect['left'], rect['top'], rect['left']+rect['width'], rect['top']+rect['height'],outline="blue")
-            #imageFrame.pack()
-        conn.close()
-        return (rect, emotion)
-    except Exception as e:
-        print("Error", e)
-        #print("[Errno {0}]".format(e))
+            conn.close()
+        except Exception as e:
+            print("Error", e)
+            #print("[Errno {0}]".format(e))
 
 class App:
     #master is the parent widget
@@ -100,15 +117,15 @@ def loadEmotions():
     result={}
     for e in emotionsAvail:
         temp= e+".gif"
-        temp=Image.open(temp)
+        c=Image.open(temp)
         #temp=temp.resize((300,300))#, Image.ANTIALIAS
 
         #temp=cv2.imread(temp)
         #temp=Image.open(temp).resize((300,300))    
         #faceEmotion = Image.fromarray(temp)
 
-        currentEmotion= ImageTk.PhotoImage(image=temp.resize((300,300),Image.ANTIALIAS ) )
-        result[e]=currentEmotion
+        #currentEmotion= ImageTk.PhotoImage(image=temp.resize((300,300),Image.ANTIALIAS ) )
+        result[e]=c
 
         #temp = ImageTk.PhotoImage(file=temp)
         #result[e]=temp
@@ -125,15 +142,23 @@ emotionPics= loadEmotions()
 print(emotionPics)
 
 
+currentEmotion=[]
+currentRect=[]
 cap = cv2.VideoCapture(0)
+concurrent=50
+q = Queue(concurrent * 2)
+for i in range(concurrent):
+    t = Thread(target=sendFace)
+    t.daemon = True
+    t.start()
 
 def drawFace(imageFrame, emotion, top, left):
 
     imageFrame.create_image(left, top, anchor=NW, image=emotion)#50,50, anchor="nw", top,left 
 
-def show_frame(ct, faceRect, currentEmotion):
+def show_frame(ct):
     
-    global emotionPics
+    global emotionPics, currentEmotion, currentRect
 
     ct+=1
     _, frame = cap.read()
@@ -147,14 +172,19 @@ def show_frame(ct, faceRect, currentEmotion):
 
     if(ct==5):
         #print("sending image")
-        cv2.imwrite("hi.png", cv2image)
-        cv2image=cv2.imread("hi.png")
-        rect, emotion=sendFace(cv2image)
-        if rect!=0:
-            faceRect=rect
-        if emotion!="":
+        #cv2.imwrite("hi.png", cv2image)
+        #cv2image=cv2.imread("hi.png")
+        try:
+            q.put(cv2image)
+            q.join()
+        except KeyboardInterrupt:
+            sys.exit(1)
+        #rect, emotion=sendFace(cv2image)
+        #if rect!=0:
+        #    faceRect=rect
+        #if emotion!="":
             #faceEmotion=emotion
-            currentEmotion=emotionPics[emotion]
+
 
             #temp=cv2.imread( emotion+".png")    
             #faceEmotion = Image.fromarray(temp)
@@ -175,20 +205,29 @@ def show_frame(ct, faceRect, currentEmotion):
             #faceEmotion = Image.fromarray(temp)
             #faceEmotion = ImageTk.PhotoImage(image=temp.resize((faceRect['width'],faceRect['height']), Image.ANTIALIAS))#.resize((1200,1000), Image.ANTIALIAS).resize((1200,1000), Image.ANTIALIAS)
                 
-    if(ct==40):
+    if(ct==10):
         ct=0
 
     imageFrame.delete("all")
     #imageFrame.create_image(600,370,image=imgtk)
     imageFrame.create_image(0,0, anchor=NW, image=imgtk)#50,50, anchor="nw", 
     imageFrame.imgtk = imgtk
-    if faceRect!=0:
-        imageFrame.create_rectangle(faceRect['left'], faceRect['top'], faceRect['left']+faceRect['width'], faceRect['top']+faceRect['height'],outline="blue")
-    if currentEmotion!="":
-        print("Create emoticon image",faceRect['left'], faceRect['top'], currentEmotion, currentEmotion.height() )
-        imageFrame.create_image(faceRect['left'], faceRect['top'], anchor=NW, image=currentEmotion)#faceRect['left'], faceRect['top']
-        imageFrame.currentEmotion=currentEmotion
-        imageFrame.image=currentEmotion
+   
+    for i in range(len(currentRect)):
+        #imageFrame.create_rectangle(currentRect[i]['left'], currentRect[i]['top'], currentRect[i]['left']+currentRect[i]['width'], currentRect[i]['top']+currentRect[i]['height'],outline="blue")
+        
+        if(isinstance(currentEmotion[i], basestring)):
+            newCurrentEmotion=[]
+            for u in range(len(currentRect)):
+                curr=emotionPics[currentEmotion[u]]
+                curr=ImageTk.PhotoImage(image=curr.resize((currentRect[u]['width'],currentRect[u]['height']),Image.ANTIALIAS ) )
+                newCurrentEmotion.append(curr)
+            currentEmotion=newCurrentEmotion
+
+        #print("Create emoticon image",currentRect['left'], currentRect['top'], currentEmotion, currentEmotion.height() )
+        imageFrame.create_image(currentRect[i]['left'], currentRect[i]['top'], anchor=NW, image=currentEmotion[i])#faceRect['left'], faceRect['top']
+        imageFrame.currentEmotion=currentEmotion[i]
+        imageFrame.image=currentEmotion[i]
         #drawFace(imageFrame,faceEmotion, faceRect['top'], faceRect['left'])
     
     #imageFrame.configure(image=imgtk)
@@ -196,12 +235,12 @@ def show_frame(ct, faceRect, currentEmotion):
     #imageFrame.create_image(50,50, anchor="nw", image=cv2image)
     #imageFrame.pack()
 
-    imageFrame.after(50, show_frame, ct, faceRect, currentEmotion) 
+    imageFrame.after(50, show_frame, ct) 
 
     #lmain.imgtk = imgtk
     #lmain.configure(image=imgtk)
     #lmain.after(50, show_frame, ct) 
 
-show_frame(0, 0, "")
+show_frame(0)
 root.mainloop() #runs till window exited/frame quitted
 root.destroy() # optional;only needed under certain dev enviros, explicitly destroys main window
